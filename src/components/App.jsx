84a96fc15b1f6e4e774d99c1f925506eedb9a50a
Bharat1002 <bj6353397686@gmail.com>
2021-08-10 18:48:51 +0530

    Full Keeper App Project Without database

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNew(note){
+        setNotes((prevValue) => {
+            return [...prevValue, note];
+        })
+    }
+
+    function deleteNote(id){
+        return setNotes((prevValue) => {
+            return prevValue.filter((note, index) => {
+                return index !== id;
+            })
+        })
+    }
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea
+      onAdd={addNew}
+      />
+      {notes.map((note, index) => {
+          return <Note
+          onDelete={deleteNote}
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          />;
+      })}
+      
       <Footer />
     </div>
   );
