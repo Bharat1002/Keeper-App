a19bd852206f532b7f9681a9cd934a82cb5adf04
Bharat1002 <bj6353397686@gmail.com>
2021-08-10 18:18:58 +0530

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import Notes from "../Notes";
+import CreateArea from "./CreateArea";
 
-
-function App(){
-    return (
-        <div>
-            <Header />
-            
-            {Notes.map( props => 
-                <Note 
-                    key={props.key}
-                    title={props.title}
-                    content={props.content}
-                />
-              )}
-                     
-            <Footer />
-        </div>
-    );
-};
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
